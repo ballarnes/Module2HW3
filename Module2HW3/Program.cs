5e34b2e5e8814c08971cdac5b24cbaaab0670f78
Aleksey Veselovskiy <ballarnes@gmail.com>
2021-09-14 19:57:49 +0300

    add an empty project + stylecop.

---
+++ Module2HW3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module2HW3
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
